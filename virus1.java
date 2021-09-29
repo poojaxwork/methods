a73c03fc1f18d92c5a4bfea53269de88bc81e8ee
poojaxwork <poojac581xworkz@mail.com>
2021-09-29 15:36:35 +0530

    adding files

---
+++ virus1.java
@@ -0,0 +1,16 @@
+class Virus
+{
+public static void Printpatdetails(String name,int age,String place,
+String admittedto,int infecteddays,String alive,String discharge)
+{
+	System.out.println(name);
+	System.out.println(age);
+	System.out.println(place);
+	System.out.println(admittedto);
+	System.out.println(infecteddays);
+	System.out.println(alive);
+	System.out.println(discharge);
+	
+	
+}
+}
